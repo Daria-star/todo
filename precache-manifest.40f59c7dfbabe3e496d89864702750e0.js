f4c069373534a8752758f6cf52f9c262f2f0ef4b
Daria <test@test.com>
2020-09-29 16:24:37 +0300

    Updates

---
+++ precache-manifest.40f59c7dfbabe3e496d89864702750e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "459fd9e9ac9ebb058f8e67f75e7e4cb2",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "7b1b0341541902598550",
+    "url": "/todo/static/css/main.3cec09eb.chunk.css"
+  },
+  {
+    "revision": "a8058694fd0b35b53c81",
+    "url": "/todo/static/js/2.5c78ee14.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo/static/js/2.5c78ee14.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b1b0341541902598550",
+    "url": "/todo/static/js/main.37401375.chunk.js"
+  },
+  {
+    "revision": "da7c3f7f7e5963fe74b8",
+    "url": "/todo/static/js/runtime-main.74315f96.js"
+  }
+]);
